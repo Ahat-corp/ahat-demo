2a4dc837d581890309b007ba0a71b708f47a0511
RoFlush <roflush@pm.me>
2020-07-29 15:08:25 -0400

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,40 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="gray"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Asshats lo"
+          class="shrink mr-2"
+          contain
+          src="https://images.evetech.net/Corporation/98566699_64.png"
+          transition="scale-transition"
+          width="40"
+        />
+      </div>
+    </v-app-bar>
+
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
